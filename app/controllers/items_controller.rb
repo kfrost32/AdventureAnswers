2323331889196dafa5742de2da5a63b400630fb3
Kyle Frost <kylefrost@Kyles-MacBook-Pro.local>
2017-08-29 16:30:20 -0600

    client lowercase

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,11 +29,11 @@ class ItemsController < ApplicationController
 
     if @item.save
       redirect_to @item, notice: 'Item was successfully created.'
-
+      client.update('Posted on twitter!')
     else
       render :new
     end
-    Client.update('Posted on twitter!')
+
   end
 
   def update
